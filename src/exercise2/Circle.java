b49edf2d9fd04ae9e7b7ac6b5bcfb2ada481d062
alisadermen <alisadermen@gmail.com>
2020-10-07 15:55:46 +0000

    ubung 2

---
+++ src/exercise2/Circle.java
@@ -0,0 +1,11 @@
+package exercise2;
+import java.util.Scanner;
+public class Circle {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter the radius: ");
+    int radius = input.nextInt();
+    System.out.println("Circumference: " + 2*radius*Math.PI);
+    System.out.println("Area: " + Math.sqrt(radius)*Math.PI);
+  }
+}
